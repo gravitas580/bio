220a3ba3e75b9c5a96314e2bd87ec4c488ddc5ca
sunaga <sunaga@example.com>
2025-05-29 01:50:37 +0900

    リンク外だし対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,5 @@
 import './App.css'
-
-const links = [
-  {
-    name: 'X',
-    href: 'https://x.com/gravitas580',
-    icon: '/icons/x.svg',
-  },
-  {
-    name: 'Qiita',
-    href: 'https://qiita.com/Gravitas',
-    icon: '/icons/qiita.svg',
-  },
-  {
-    name: 'SpeakerDeck',
-    href: 'https://speakerdeck.com/hibikisuaga',
-    icon: '/icons/speakerdeck.svg',
-  },
-  {
-    name: 'GitHub',
-    href: 'https://github.com/gravitas580',
-    icon: '/icons/github.svg',
-  },
-  {
-    name: 'LinkedIn',
-    href: '#', // 後でURL追加してね
-    icon: '/icons/linkedin.svg',
-  },
-]
+import { links } from './data/links'
 
 function App() {
   return (
---
+++ src/data/links.ts
@@ -0,0 +1,29 @@
+// src/data/links.ts
+
+export const links = [
+  {
+    name: "X",
+    href: "https://x.com/gravitas580",
+    icon: "/icons/x.svg",
+  },
+  {
+    name: "Qiita",
+    href: "https://qiita.com/Gravitas",
+    icon: "/icons/qiita.svg",
+  },
+  {
+    name: "SpeakerDeck",
+    href: "https://speakerdeck.com/hibikisuaga",
+    icon: "/icons/speakerdeck.svg",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/gravitas580",
+    icon: "/icons/github.svg",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/hibikisunaga/",
+    icon: "/icons/linkedin.svg",
+  },
+];
